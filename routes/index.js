d2847cc58d0a482669d19ce0cbdc12b4bd577403
Micaela Tapia <mclmrls29@gmail.com>
2025-05-17 19:42:15 -0400

    fixed error

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-// router.get('/', (req, res) => {
-//     //#swagger.tags=['Hello World']
-//     req.setEncoding('hello world');
-// });
+router.get('/', (req, res) => {
+    //#swagger.tags=['Hello World']
+    req.setEncoding('hello world');
+});
 router.use('/api-docs', require('./swagger'));
 
 router.use('/users', require('./users'));
